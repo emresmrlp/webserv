834a46eb8623f96762c76ccd9f6ad3dda83057d9
berkeldemir <berkeldemir@outlook.com>
2026-06-29 13:24:13 +0300

    refactor(main): test outputs removed

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 15:49:14 by ysumeral          #+#    #+#             */
-/*   Updated: 2026/06/29 01:31:24 by ysumeral         ###   ########.fr       */
+/*   Updated: 2026/06/29 13:21:13 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,44 +21,46 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        std::cout << "Correct usage: ./webserv [configuration file]" << std::endl;
-        return (1);
-    }
-    
-    signal(SIGPIPE, SIG_IGN);
+	std::string filename = "conf/default.conf";
 
-    config::Parser parser(argv[1]);
-    try
-    {
-        parser.parse();
-        const std::vector<config::ConfigServer>& servers = parser.getServers();
-        std::cout << "Parser Debug: Successfully loaded " << servers.size() << " config!" << std::endl;
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Parse error: " << e.what() << std::endl;
-        return 1;
-    }
+	if (argc > 2)
+	{
+		std::cout << "Correct usage: ./webserv [configuration file]" << std::endl;
+		return (1);
+	}
+	else if (argc != 1)
+		filename = argv[1];
+	
+	signal(SIGPIPE, SIG_IGN);
 
-    if (parser.getServers().empty())
-    {
-        std::cerr << "Fatal Error: No valid server blocks found in config file." << std::endl;
-        return 1;
-    }
+	config::Parser parser(filename);
+	try
+	{
+		parser.parse();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Parse error: " << e.what() << std::endl;
+		return 1;
+	}
 
-    try
-    {
-        core::ServerHandler serverHandler;
-        serverHandler.init(parser.getServers());
-        serverHandler.run();
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << "Server Error: " << e.what() << std::endl;
-        return (1);
-    }
-    
-    return (0);
+	if (parser.getServers().empty())
+	{
+		std::cerr << "Fatal Error: No valid server blocks found in config file." << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		core::ServerHandler serverHandler;
+		serverHandler.init(parser.getServers());
+		serverHandler.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Server Error: " << e.what() << std::endl;
+		return (1);
+	}
+	
+	return (0);
 }
